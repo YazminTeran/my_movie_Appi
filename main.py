c13e0c9658d7ef2fccf6361c9b28244e83b81f2f
Yazmin Teran <diosenyteran@gmail.com>
2024-09-17 23:12:36 -0500

    abordamos hasta la secion 2

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app. title= "Mi aplicacion con fastApi"
 app.version= "0.0.1"
 
+movies_list = [
+    {
+        "id": 1,
+        "title": "Palpito",
+        "overview":"trafico de organos",
+        "year": 2023,
+        "rating": 10.0
+    
+    },
+     {
+        "id": 2,
+        "title": "manifiesto",
+        "overview":"vuelo perdido  cinco años",
+        "year": 2023,
+        "rating": 10.0
+    
+    }
+]
+
 @app.get('/',tags=["Home"])
-def read_root():
-    return {"Hello": "World"}
+def messaje ():
+    return HTMLResponse ('<h1>Hello word</h1>')
+@app.get('/movies',tags=["Movies"])
+def get_movies():
+    return movies_list
 
 
 
