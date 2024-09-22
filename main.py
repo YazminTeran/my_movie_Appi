ead43d3bbd8d0c2969c024fdb7ce529dc4e650d8
Yazmin Teran <diosenyteran@gmail.com>
2024-09-22 00:15:23 -0500

    list movie

--- main.py
+++ main.py
@@ -1,100 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Body
 from fastapi.responses import HTMLResponse
+from movis_list import movies_list
 
 app = FastAPI()
 app. title= "Mi aplicacion con fastApi"
 app.version= "0.0.1"
 
-movies_list = [
-    {
-        "id": 1,
-        "title": "Palpito",
-        "overview":"serie de trafico de organos",
-        "year": 2023,
-        "rating": 10.0
-    
-    },
-     {
-        "id": 2,
-        "title": "manifiesto",
-        "overview":"serie de vuelo perdido  cinco años",
-        "year": 2023,
-        "rating": 10.0
-    
-    } ,
 
-    {    "id": 3,
-        "title": "brigerton",
-        "overview":"serie ",
-        "year": 2024,
-        "rating": 10.0
-    
-    },
-
-    {    "id": 4,
-        "title": "el pajaro loco",
-        "overview":"el pajaro loco se va a acampar",
-        "year": 2024,
-        "rating": 10.0
-    
-    },
-
-    {    "id": 5,
-        "title": "sing 1",
-        "overview":"concurso musical",
-        "year": 2016,
-        "rating": 10.0
-    
-    },
-
-    {    "id": 6,
-        "title": "sing 2",
-        "overview":"ven y canta de nuevo",
-        "year": 2021,
-        "rating": 10.0
-    
-    },
-   
-    {    "id": 7,
-        "title": "chicas pesadas",
-        "overview":"comeia fresca y deivertida",
-        "year": 2004,
-        "rating": 10.0
-    
-    },
-    {    "id": 8,
-        "title": "kung fu panda",
-        "overview":"el guerrero draon",
-        "year": 2023,
-        "rating": 10.0
-    
-    },
-    {    "id": 9,
-        "title": "la era de hielo 3",
-        "overview":"Nace morita",
-        "year": 2009,
-        "rating": 10.0
-    
-    },
-    {    "id": 10,
-        "title": "la era de hielo",
-        "overview":"Choquee de mundos",
-        "year": 2016,
-        "rating": 10.0
-    
-    }
-]
 
 @app.get('/',tags=["Home"])
 def messaje ():
     return HTMLResponse ('<h1>Hello word</h1>')
+
 @app.get('/movies',tags=["Movies"])
 def get_movies():
     return movies_list
 
-@app.get('/movies'/{id}'',tags=["Movies"])
+@app.get('/movies/{id}' , tags=["Movies"])
 def get_movies(id:int):
     for item in movies_list:
         if item['id']== id:
             return item 
     return[]
+
+@app.get('/movies/' , tags=["Movies"])
+def get_movies_by_category(category:str,year:int):
+    movies_list_found=[]
+    for item in movies_list:
+        if item["year"]==year:
+            movies_list_found.append(item)
+    return movies_list_found
+
+@app.post('/movies/' , tags=["Movies"])
+def create_ovie(id: int= Body(), title: str = Body(), overview: str=Body(),yeard:int= Body(), rating:float=Body(),category:str=Body()):
+  
+    movies_list.append({
+        "id": id,
+        "title":title,
+        "overview": overview,
+        "yeard":yeard,
+        "rating":rating,
+        "category":category})
+    return movies_list
---
+++ movis_list.py
@@ -0,0 +1,89 @@
+movies_list = [
+    {
+        "id": 1,
+        "title": "Palpito",
+        "overview":"serie de trafico de organos",
+        "year": 2023,
+        "rating": 10.0,
+        "category": "Ficcion"
+    
+    },
+     {
+        "id": 2,
+        "title": "manifiesto",
+        "overview":"serie de vuelo perdido  cinco años",
+        "year": 2023,
+        "rating": 10.0,
+        "category":"ficcion"
+    
+    } ,
+
+    {    "id": 3,
+        "title": "brigerton",
+        "overview":"serie ",
+        "year": 2024,
+        "rating": 10.0,
+        "category":"drama"
+    },
+
+    {    "id": 4,
+        "title": "el pajaro loco",
+        "overview":"el pajaro loco se va a acampar",
+        "year": 2024,
+        "rating": 10.0,
+        "category": "comedia"
+        
+    },
+
+    {    "id": 5,
+        "title": "sing 1",
+        "overview":"concurso musical",
+        "year": 2016,
+        "rating": 10.0,
+        "category": "animados"
+    
+    },
+
+    {    "id": 6,
+        "title": "sing 2",
+        "overview":"ven y canta de nuevo",
+        "year": 2021,
+        "rating": 10.0,
+        "category":"animados"
+    
+    },
+   
+    {    "id": 7,
+        "title": "chicas pesadas",
+        "overview":"comedia fresca y deivertida",
+        "year": 2004,
+        "rating": 10.0,
+        "category":"comedia"
+    
+    },
+    {    "id": 8,
+        "title": "kung fu panda",
+        "overview":"el guerrero draon",
+        "year": 2023,
+        "rating": 10.0,
+        "category": "animados"
+
+    
+    },
+    {    "id": 9,
+        "title": "la era de hielo 3",
+        "overview":"Nace morita",
+        "year": 2009,
+        "rating": 10.0,
+        "category": "animado"
+    
+    },
+    {    "id": 10,
+        "title": "la era de hielo",
+        "overviewc":"Choquee de mundos",
+        "year": 2016,
+        "rating": 10.0,
+        "category": "animado"
+    
+    }
+]
